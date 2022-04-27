0b8d4bb3c61b286b1d498dfd17872de55b65e5e6
DavidVentura <davidventura27@gmail.com>
2022-04-27 20:32:33 +0200

    record as mp4

--- rec.sh
+++ rec.sh
@@ -2,4 +2,4 @@ set -eu
 TITLE="Pico Pico"
 WINDOW_XID=$(xwininfo -tree -root -all | egrep "$TITLE" | sed -e 's/^ *//' | cut -d\  -f1)
 echo $WINDOW_XID
-gst-launch-1.0 -e ximagesrc xid=$WINDOW_XID ! video/x-raw,framerate=30/1 ! videoconvert ! queue ! x264enc ! matroskamux ! filesink location=video.mkv
+gst-launch-1.0 -e ximagesrc xid=$WINDOW_XID ! video/x-raw,framerate=30/1 ! videoconvert ! queue ! x264enc ! mp4mux ! filesink location=video.mp4

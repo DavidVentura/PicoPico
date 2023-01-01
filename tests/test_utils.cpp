66425c280ea139c839a0f9891328aeab24494e09
DavidVentura <davidventura27@gmail.com>
2023-01-01 16:02:37 +0100

    add warning on overwriting tests; fix test file mode; update helloworld test

--- tests/test_utils.cpp
+++ tests/test_utils.cpp
@@ -2,12 +2,13 @@
 
 bool compare_buffer(const char* in_file, uint8_t* buf, uint16_t buf_len, bool dump_values) {
     if (dump_values) {
-        FILE *fw = fopen(in_file, "wb");
+        FILE *fw = fopen(in_file, "w");
         fwrite(buf, sizeof(uint8_t), buf_len, fw);
         fclose(fw);
+        printf("[WARN] Overriding values for %s\n", in_file);
     }
 
-    FILE *f = fopen(in_file, "rb");
+    FILE *f = fopen(in_file, "r");
     fseek(f, 0, SEEK_END);
     uint16_t bytes_read = ftell(f);
     fseek(f, 0, SEEK_SET);
@@ -20,8 +21,8 @@ bool compare_buffer(const char* in_file, uint8_t* buf, uint16_t buf_len, bool du
 
     for(uint16_t i=0; i<bytes_read; i++) {
         if(buf[i] != golden_data[i]) {
-	    return false;
-	}
+            return false;
+        }
     }
     free(golden_data);
     return true;

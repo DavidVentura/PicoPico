1bb68c9ca3ca7f080b81b49c4efc2b1f0b68d3c7
DavidVentura <davidventura27@gmail.com>
2023-03-11 15:45:04 +0100

    fix menu test

--- tests/test_menu.cpp
+++ tests/test_menu.cpp
@@ -4,16 +4,24 @@
 
 bool handle_input() {
     static uint8_t queryCounter = 0;
+    static uint8_t downCounter = 0;
+    buttons_frame[3] = 0;
+    if (downCounter == 0) {
+	    // hold down for 1 frame
+	    buttons_frame[3] = 1;
+	    downCounter = 1;
+	    printf("down!");
+    }
     return queryCounter++ == 5; // wants to quit
 }
 
-void test_hello_world() {
+void test_menu() {
     drawMenu();
     assert(compare_buffer("../tests/data/menu.bin", frontbuffer, sizeof(frontbuffer), false));
 }
 
 int main() {
     engine_init();
-    test_hello_world();
+    test_menu();
     return 0;
 }

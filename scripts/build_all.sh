4fcc1b390b25aff1ec0dce21020b56137b4f9781
DavidVentura <davidventura27@gmail.com>
2023-05-29 01:46:52 +0200

    bump to use alsa libs

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eu
 # No dependencies for: ESP32 PC, RAWDRAW, ANDROID 
-for target in THREEDS ANDROID TEST; do
-	docker run --user $(id -u):$(id -g) -v "$PWD:/src" davidv27/picopico-builder:0.0.4 /bin/bash -c "set -x; rm -rf build-$target; mkdir -p build-$target; cd build-$target; cmake .. -DANDROIDSDK=/android_sdk -DBACKEND=$target && make -j"
+for target in RAWDRAW THREEDS ANDROID TEST; do
+	docker run --user $(id -u):$(id -g) -v "$PWD:/src" davidv27/picopico-builder:0.0.5 /bin/bash -c "set -x; rm -rf build-$target; mkdir -p build-$target; cd build-$target; cmake .. -DANDROIDSDK=/android_sdk -DBACKEND=$target && make -j"
 done

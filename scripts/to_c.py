ed691a541b5bd339048e227630873974e6fed050
DavidVentura <davidventura27@gmail.com>
2023-07-02 19:23:49 +0200

    pipe through black

--- scripts/to_c.py
+++ scripts/to_c.py
@@ -98,23 +98,25 @@ def process_cart(name: str, data: bytes) -> GameCart:
             sections.setdefault(section, [])
             continue
         elif section is None:
-            section = 'marker'
+            section = "marker"
             sections.setdefault(section, [])
             continue
         sections[section].append(bytes(line))
 
-    sections[LUA_HEADER] = compile_lua_to_bytecode(luac, b'\n'.join(sections.get(LUA_HEADER, b'')))
+    sections[LUA_HEADER] = compile_lua_to_shared_object(b"\n".join(sections.get(LUA_HEADER, b"")))
 
-    gc = GameCart(name=name,
-                  code=sections.get(LUA_HEADER, b''),
-                  gfx=to_char_value(b''.join(sections.get(b'__gfx__', []))),
-                  gff=hex_digits_to_bytes(b''.join(sections.get(b'__gff__', []))),
-                  label=to_char_value(b''.join(sections.get(LABEL_HEADER, []))),
-                  map=hex_digits_to_bytes(b''.join(sections.get(b'__map__', []))),
-                  sfx=b''.join(sections.get(b'__sfx__', [])),
-                  music=b'\n'.join(sections.get(b'__music__', []))
-                  )
+    gc = GameCart(
+        name=name,
+        code=sections.get(LUA_HEADER, b""),
+        gfx=to_char_value(b"".join(sections.get(b"__gfx__", []))),
+        gff=hex_digits_to_bytes(b"".join(sections.get(b"__gff__", []))),
+        label=to_char_value(b"".join(sections.get(LABEL_HEADER, []))),
+        map=hex_digits_to_bytes(b"".join(sections.get(b"__map__", []))),
+        sfx=b"".join(sections.get(b"__sfx__", [])),
+        music=b"\n".join(sections.get(b"__music__", [])),
+    )
     return gc
+
 
 def hex_digits_to_bytes(data: bytes) -> bytes:
     """
@@ -122,15 +124,17 @@ def hex_digits_to_bytes(data: bytes) -> bytes:
     """
     ret = []
     for i in range(0, len(data), 2):
-        ret.append(_to_char(data[i]) << 4 | _to_char(data[i+1]))
+        ret.append(_to_char(data[i]) << 4 | _to_char(data[i + 1]))
     return bytes(ret)
+
 
 def _to_char(c: int) -> int:
     if c >= 97:
-        return c - 87 # 'a'-'f' -> 10-15
+        return c - 87  # 'a'-'f' -> 10-15
     elif c >= 48 and c <= 57:
-        return c - 48 # '0'-9' -> 0-9
+        return c - 48  # '0'-9' -> 0-9
     raise ValueError(f"Can't convert {c}")
+
 
 def to_char_value(data: bytes) -> bytes:
     """
@@ -138,46 +142,51 @@ def to_char_value(data: bytes) -> bytes:
     """
     return bytes(map(_to_char, data))
 
-def path_to_identifier(p: Path) -> str:
-    pname = os.path.basename(os.path.dirname(p)).replace('.', '_').replace('-', '_').replace('/', '_')
-    bname = os.path.basename(p).replace('.', '_').replace('-', '_')
-    return f'{pname}_{bname}'
 
-def _chunk(data: bytes, join: str='\n', brace_enclosed=True) -> str:
+def path_to_identifier(p: Path) -> str:
+    pname = os.path.basename(os.path.dirname(p)).replace(".", "_").replace("-", "_").replace("/", "_")
+    bname = os.path.basename(p).replace(".", "_").replace("-", "_")
+    return f"{pname}_{bname}"
+
+
+def _chunk(data: bytes, join: str = "\n", brace_enclosed=True) -> str:
     output = []
 
     if brace_enclosed:
-        output.append('{')
+        output.append("{")
 
     if data:
         for chunk in chunked(data, 32):
-            output.append('  ' + ', '.join([f'0x{b:02x}' for b in chunk]) + ',')
+            output.append("  " + ", ".join([f"0x{b:02x}" for b in chunk]) + ",")
 
     if brace_enclosed:
-        output.append('}')
+        output.append("}")
     return join.join(output)
 
-def _type(varname: str, uniq: str, data: bytes) -> str:
-    return f'const uint8_t {varname}_{uniq}[] = {_chunk(data)}'
 
-def parse_cart(fname: Path, debug: bool=False, luac: str=''):
+def _type(varname: str, uniq: str, data: bytes) -> str:
+    return f"const uint8_t {varname}_{uniq}[] = {_chunk(data)}"
+
+
+def parse_cart(fname: Path, debug: bool = False):
     if not os.path.isfile(fname):
         print(f"'{fname}' does not exist or is not a file")
         sys.exit(1)
 
-    bname = path_to_identifier(fname).replace('carts_', '')
+    bname = path_to_identifier(fname).replace("carts_", "")
 
-    with open(fname, 'rb') as fd:
+    with open(fname, "rb") as fd:
         data = fd.read()
-    cart = process_cart(luac, bname, data)
-    output = textwrap.dedent(f'''
+    cart = process_cart(bname, data)
+    output = textwrap.dedent(
+        f"""
     {_type('code', bname, cart.code)};
     {_type('gfx', bname, cart.gfx)};
     {_type('gff', bname, cart.gff)};
     {_type('sfx', bname, cart.sfx)};
     {_type('map', bname, cart.map)};
     {_type('label', bname, cart.label)};
-    const GameCart cart_{bname} = {{
+    GameCart cart_{bname} = {{
         .name_len={len(cart.name)},
         .name="{cart.name}",
         .code_len={len(cart.code)},
@@ -193,67 +202,79 @@ def parse_cart(fname: Path, debug: bool=False, luac: str=''):
         .label_len={len(cart.label)},
         .label=label_{bname},
     }};
-    ''')
+    """
+    )
     return output
 
-def parse(luac: str, fname: Path, process_as: ProcessType, debug: bool=False):
+
+def parse(fname: Path, process_as: ProcessType, debug: bool = False):
     if not os.path.isfile(fname):
         print(f"'{fname}' does not exist or is not a file")
         sys.exit(1)
 
     bname = path_to_identifier(fname)
 
-    with open(fname, 'rb') as fd:
+    with open(fname, "rb") as fd:
         data = fd.read()
 
     output = []
     if process_as is ProcessType.RAW:
         # add a null byte, length is unknown
-        data = to_char_value(b''.join(data.splitlines()))
-        output.append(f'const uint16_t {bname}_len = {len(data)};')
+        data = to_char_value(b"".join(data.splitlines()))
+        output.append(f"const uint16_t {bname}_len = {len(data)};")
     else:
-        data = compile_lua_to_bytecode(luac, data)
+        data = compile_lua_to_shared_object(data)
 
     initial_len = len(data)
     processed_data = data
     new_len = len(processed_data)
 
-    output.append(f'const uint8_t {bname}[] = ')
-    output.append(_chunk(processed_data, join='\n'))
-    output.append(';')
+    output.append(f"const uint8_t {bname}[] = ")
+    output.append(_chunk(processed_data, join="\n"))
+    output.append(";")
 
     if process_as is ProcessType.COMPILE:
-        output.append(f'const uint16_t {bname}_len = {len(data)};')
+        output.append(f"const uint16_t {bname}_len = {len(data)};")
     if debug:
-        print(f'[{bname}] {initial_len=} {new_len=}', file=sys.stderr)
-    return '\n'.join(output)
+        print(f"[{bname}] {initial_len=} {new_len=}", file=sys.stderr)
+    return "\n".join(output)
+
 
 def parse_args():
     p = argparse.ArgumentParser()
-    p.add_argument('--emit-stdlib', action='store_true')
-    p.add_argument('--cart-prefix', required=False, default='')
-    p.add_argument('--luac', required=False, default='lua/luac')
-    p.add_argument('directory')
+    p.add_argument("--emit-stdlib", action="store_true")
+    p.add_argument("--cart-prefix", required=False, default="")
+    p.add_argument("directory")
     return p.parse_args()
+
 
 def main():
     args = parse_args()
     debug = True
     if args.emit_stdlib:
-        print(parse(args.luac, Path('stdlib/stdlib.lua'), ProcessType.COMPILE, debug))
-        print(parse(args.luac, Path('artifacts/font.lua'), ProcessType.RAW, debug))
-        print(parse(args.luac, Path('artifacts/hud.p8'), ProcessType.RAW, debug))
+        # stdlib is unused
+        # print(parse(Path("stdlib/stdlib.lua"), ProcessType.COMPILE, debug))
+        print(parse(Path("artifacts/font.lua"), ProcessType.RAW, debug))
+        print(parse(Path("artifacts/hud.p8"), ProcessType.RAW, debug))
 
     games = []
-    for f in Path(args.directory).glob('*.p8'):
+    for f in Path(args.directory).glob("*.p8"):
+        if f.stem in ['celeste', 'aalpaca', '']:
+            continue
+        print(f"Parsing {f}", file=sys.stderr)
+        try:
+            print(parse_cart(f, debug))
+        except ValueError:
+            print('  failed', file=sys.stderr)
+            continue
         games.append(path_to_identifier(f))
-        print(parse_cart(f, debug, args.luac))
 
-    print(f'GameCart {args.cart_prefix}carts[] = {{')
+    print(f"GameCart* {args.cart_prefix}carts[] = {{")
     for game in games:
-        game_name = game.replace('carts_', '')
-        print(f'cart_{game_name},')
-    print('};')
+        game_name = game.replace("carts_", "")
+        print(f"&cart_{game_name},")
+    print("};")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
